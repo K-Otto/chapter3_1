8fe7a2390dd8f13c2344ea2465187048547ace04
K-Otto <karl1256@yahoo.com>
2015-02-20 19:40:49 +0200

    Collection Commit2

--- collection1/src/main/java/ac/za/cput/chapter3/CollectionApp.java
+++ collection1/src/main/java/ac/za/cput/chapter3/CollectionApp.java
@@ -1,14 +1,14 @@
 package ac.za.cput.chapter3;
 
 import java.util.ArrayList;
-
+import java.util.Collection;
 
 
 /**
  * Created by student on 2015/02/20.
  */
 public interface CollectionApp {
-    public ArrayList stringCollection = new ArrayList();
+    public Collection<Players> stringCollection = new ArrayList<Players>();
 
     public void add2(int s, String n, String t);
 
